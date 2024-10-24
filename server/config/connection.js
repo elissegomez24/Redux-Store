c0cbd278811bccd0f37745f3e070770d3d0667e1
elissegomez24 <elissegomez24@gmail.com>
2024-10-24 11:44:43 -0400

    updated MongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://elisse:AgFL8HGoAwIhCB1H@bookstore.jml8r.mongodb.net/bookstore?retryWrites=true&w=majority&appName=bookstore');
 
 module.exports = mongoose.connection;
